1adf5569f90a58dbda7273c1ac353f7b76df99f8
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2021-10-06 11:13:47 +0530

    Create Q8 ---> Sum of even and odd num.cpp

---
+++ Wipro NLTH/Q8 ---> Sum of even and odd num.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    long int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+
+    int sumEven=0;
+    int sumOdd=0;
+    int rem=0;
+    int diff=0;
+    
+    while(num!=0)
+    {
+        rem = num%10;
+        if(rem%2 == 0)
+        {
+            sumEven += rem;
+        }
+        else
+        {
+            sumOdd += rem;
+        }
+        
+         num = num/10;
+    }
+    
+    if(sumOdd>sumEven)
+    {
+        diff = (sumOdd-sumEven);
+    }
+    else
+    {
+        diff = (sumEven-sumOdd);
+    }
+    
+    cout<<"Difference is : "<<diff;
+    
+    return 0;
+}
