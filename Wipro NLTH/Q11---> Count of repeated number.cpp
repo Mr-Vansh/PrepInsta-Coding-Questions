eefdecce3694d69eaf8e71168806d32ef970b149
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2021-10-06 11:18:00 +0530

    Create Q11---> Count of repeated number.cpp

---
+++ Wipro NLTH/Q11---> Count of repeated number.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+     int n;
+     cout<<"Enter the number : ";
+     cin>>n;
+     
+     int targetElement;
+     cout<<"Enter target element : ";
+     cin>>targetElement;
+     
+     int count=0;
+     int rem=0;
+     
+     while(n!=0)
+     {
+         rem = n%10;
+         if(rem == targetElement){
+             count++;
+         }
+         n = n/10;
+     }
+     
+     cout<<"Count : "<<count;
+     
+    return 0;
+}
